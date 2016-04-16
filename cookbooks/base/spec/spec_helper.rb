4198d0407e09f70ba46bb11512f47c5a806486b2
Ashish Bista <b@ashishbista.com>
2016-04-15 20:25:00 -0400

    Spec for base::default recipe

--- cookbooks/base/spec/spec_helper.rb
+++ cookbooks/base/spec/spec_helper.rb
@@ -1,2 +1,29 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  # Specify the path for Chef Solo to find cookbooks (default: [inferred from
+  # the location of the calling spec file])
+  config.cookbook_path = '../../../cookbooks'
+
+  # Specify the path for Chef Solo to find roles (default: [ascending search])
+  config.role_path = '../../../roles'
+
+  # Specify the path for Chef Solo to find environments (default: [ascending search])
+  config.environment_path = '../../../environments'
+
+  # Specify the path for Chef Solo file cache path (default: nil)
+  config.file_cache_path = '../../../cache'
+
+  # Specify the Chef log_level (default: :warn)
+  config.log_level = :warn
+
+  # Specify the path to a local JSON file with Ohai data (default: nil)
+  # config.path = 'ohai.json'
+
+  # Specify the operating platform to mock Ohai data from (default: nil)
+  config.platform = 'ubuntu'
+
+  # Specify the operating version to mock Ohai data from (default: nil)
+  config.version = '14.04'
+end
--- cookbooks/base/spec/unit/recipes/default_spec.rb
+++ cookbooks/base/spec/unit/recipes/default_spec.rb
@@ -4,19 +4,57 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-require 'spec_helper'
+require_relative '../../spec_helper'
 
 describe 'base::default' do
 
   context 'When all attributes are default, on an unspecified platform' do
-
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
+    before do
     end
 
+
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new do |node, server|
+        server.create_data_bag('users', {
+          'deploy' => {
+            'password' => 'abc123',
+            'ssh_keys' => [
+              {'ashish-laptop' => 'ssh'}
+            ],
+            'shell' => '/bin/bash'
+          }
+        })
+      end
+      runner.converge(described_recipe)
+    end
     it 'converges successfully' do
       chef_run # This should not raise an error
+    end
+
+    it 'installs htop' do
+      expect(chef_run).to install_package('htop')
+    end
+
+    it 'creates deploy user' do
+      expect(chef_run).to create_user('deploy').with(password: 'abc123', shell: '/bin/bash' )
+    end
+
+    it 'creates home direcotry for deploy user' do
+       expect(chef_run).to create_directory('/home/deploy')
+    end
+
+    it 'creates .ssh directory for deploy user' do
+      expect(chef_run).to create_directory('/home/deploy/.ssh')
+    end
+
+    it 'creates authorized_keys template' do
+      expect(chef_run).to create_template('/home/deploy/.ssh/authorized_keys').with(
+        owner:   'deploy'
+      )
+    end
+
+    it 'makes deploy user sudoer' do
+      expect(chef_run).to modify_group('sudo').with(members: ['deploy'], append: true)
     end
 
   end
